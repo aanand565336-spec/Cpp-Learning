e669fd76b3796f815e8669a31342f7e3167d0353
aanand565336-spec <aanand565336@gmail.com>
2026-08-03 10:39:34 +0530

    Add: User input with cin

---
+++ 03_user_input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Tera naam kya hai?" << endl;
+    cin >> name;
+
+    cout << "Teri age kya hai?" << endl;
+    cin >> age;
+
+    cout << "Hello " << name << "!" << endl;
+    cout << "Tu " << age << " saal ka hai" << endl;
+
+    return 0;
+}
