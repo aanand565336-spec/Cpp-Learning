e6a10c1a92baa32bab10a01e2032a9dbe662ae34
aanand565336-spec <aanand565336@gmail.com>
2026-08-03 10:39:27 +0530

    Add: Variables exercise - name, food, age

---
+++ 02_variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Aanad";
+    string food = "Biryani";
+    int age = 14;
+
+    cout << "My name is " << name << endl;
+    cout << "My favorite food is " << food << endl;
+    cout << "I am " << age << " years old" << endl;
+
+    return 0;
+}
